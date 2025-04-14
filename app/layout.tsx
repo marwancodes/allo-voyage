1a35249319d7083d7dcaefb83422439d244c1f80
marwancoder <marwane.ouarradi@gmail.com>
2025-04-15 00:54:26 +0100

    Added Scroll To Top

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Poppins } from "next/font/google";
 import "./globals.css";
 import ResponsiveNavbar from "@/components/Home/Navbar/ResponsiveNavbar";
 import Footer from "@/components/Home/Footer/Footer";
+import ScrollToTop from "@/components/Helper/ScrollToTop";
 
 const font = Poppins({
   weight: ['100','200','300','400','500','600','700','800','900'],
@@ -36,6 +37,7 @@ export default function RootLayout({
         <ResponsiveNavbar />
         {children}
         <Footer />
+        <ScrollToTop />
       </body>
     </html>
   );
---
+++ components/Helper/ScrollToTop.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import { useEffect, useState } from "react";
+import { FaArrowUp } from "react-icons/fa6";
+
+// Description: This component is used to scroll the page to the top when the user clicks on it.
+
+const ScrollToTop = () => {
+
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+        const toggleVisibility = () => {
+            if (window.scrollY > 300) {
+                setIsVisible(true);
+            } else {
+                setIsVisible(false);
+            }
+        }
+        window.addEventListener("scroll", toggleVisibility);
+        return () => window.removeEventListener("scroll", toggleVisibility);
+    }, []);
+
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    }
+
+  return (
+    <div className="fixed bottom-4 right-4 animate-pulse">
+        {isVisible && (
+            <button onClick={scrollToTop} className="bg-rose-700 text-white rounded-full h-12 w-12 flex items-center justify-center focus:outline-none cursor-pointer">
+                <FaArrowUp />
+            </button>
+        )}
+    </div>
+  )
+}
+
+export default ScrollToTop
